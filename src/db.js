a8ccb724704ad284bfad1a5196c1608a0fa8292a
Muhammad Ehsaan <35750@students.riphah.edu.pk>
2026-06-08 17:17:53 +0500

    Configure PostgreSQL SSL for Neon

--- src/db.js
+++ src/db.js
@@ -4,7 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config({ quiet: true });
 
 const connectionString = process.env.DATABASE_URL || "postgresql://postgres@localhost:5432/cashbok_dashboard";
+const needsSsl = /neon\.tech|sslmode=require|sslmode=verify-full/i.test(connectionString);
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({
+  connectionString,
+  ssl: needsSsl ? { rejectUnauthorized: false } : false
+});
 
 module.exports = pool;
